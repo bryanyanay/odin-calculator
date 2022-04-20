53782c5a6daba8ec07b7b036d63c161085541248
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-19 22:03:25 -0400

    Work on calculation functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-
-let numBtns = document.getElementById("number-buttons");
+let displayMax = 12; // the max number of characters we will display on the calculator screen 
 
 function inputNumber(e) {
     if (e.target.tagName !== "BUTTON") return;
@@ -16,4 +15,79 @@ function inputNumber(e) {
     }
 }
 
+let numBtns = document.getElementById("number-buttons");
 numBtns.addEventListener("click", inputNumber);
+
+function calcResult(a, b, operator) {
+    // accepts a and b as strings, and returns a string
+    // if result is too big, returns "TOO BIG"
+    // if result has too many decimals, truncates (doesn't round!)
+    let result;
+    a = +a;
+    b = +b;
+    switch (operator) {
+        case "+":
+            result = a+b;
+            break;
+        case "-":
+            result = a-b;
+            break;
+        case "×":
+            result = a*b;
+            break;
+        case "÷":
+            result = a/b;
+            break;
+    }
+    result.toString();
+    if (result.length > displayMax) {
+        if (result.include(".")) 
+            return result.substring(0, displayMax);
+        else 
+            return "TOO BIG";
+    }
+    return result;
+}
+function getDownButton() {
+    let operationBtns = document.getElementById("operation-buttons").children;
+    for (o of operationBtns)
+        if (o.classList.contains("down"))
+            return o;
+}
+
+function prepareOperation(e) {
+    if (e.target.tagName !== "BUTTON") return;
+
+    document.getElementById("prev-result").textContent = 
+            document.getElementById("input-field").textContent;
+
+    e.target.classList.add("down");
+
+    document.getElementById("input-field").textContent = "0";
+}
+function executeOperation() {
+    let prevResult = document.getElementById("prev-result");
+    let currInput = document.getElementById("input-field");
+    let currOperation = getDownButton();
+
+    currInput.textContent = calcResult(prevResult.textContent, currInput.textContent, currOperation.textContent);
+    prevResult.textContent = "";
+    currOperation.classList.remove("down");
+}
+
+// TODO: round the number, div by 0
+
+let operationBtns = document.getElementById("operation-buttons");
+let equalBtn = document.getElementById("equals-button");
+
+operationBtns.addEventListener("click", (e) => {
+    if (document.getElementById("prev-result").textContent) {
+        executeOperation();
+        prepareOperation(e)
+    } else {
+        prepareOperation(e);
+    }
+});
+equalBtn.addEventListener("click", () => { 
+    if (document.getElementById("prev-result").textContent) executeOperation();
+});
